a1e12f3c07353e39c5f43b89f1c93e41ac981df1
Sergey <sergomorello@yandex.ru>
2023-08-02 20:19:19 +0300

    Исправил composer.json

--- src/Init.php
+++ src/Init.php
@@ -8,6 +8,8 @@ class Init
 {
 	public static function postInstall(PackageEvent $event)
     {
-        copy('vendor/larasopp/larasopp/files/events.php', 'routes/events.php');
+        //copy('vendor/larasopp/larasopp/files/events.php', 'routes/events.php');
+		file_put_contents('test.txt', __DIR__);
+
     }
 }
